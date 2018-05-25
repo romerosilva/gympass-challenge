d851e45f3917f4a2ba777d0b59846c10c8f55ccf
Romero Silva <romero.ca.silva@gmail.com>
2018-05-24 23:56:55 -0300

    Added log_entry

---
+++ src/data/model/log_entry.py
@@ -0,0 +1,8 @@
+class LogEntry:
+    def __init(time, code, pilot, lap, lapTime, avgSpeed):
+        self.time = time
+        self.code = code
+        self.pilot = pilot
+        self.lap = lap
+        self.lapTime = lapTime
+        self.avgSpeed = avgSpeed

733a0167e11cb3644bad7e1dfadc0bf23011cfb7
Romero Silva <romero.ca.silva@gmail.com>
2018-05-24 23:55:30 -0300

    Finished implementing read_lines

--- src/data/file_reader.py
+++ src/data/file_reader.py
@@ -7,6 +7,8 @@ class FileReader:
     def read_lines(self):
         with open(self.file,'r') as f:
             self._check_if_empty_file(f)
+            for line in f:
+                yield line.strip('\n')
 
     def _check_if_empty_file(self, file):
         if os.fstat(file.fileno()).st_size == 0:
--- tests/data/test_file_reader.py
+++ tests/data/test_file_reader.py
@@ -8,3 +8,7 @@ class FileReaderTest(unittest.TestCase):
         with self.assertRaises(Exception):
             list(reader.read_lines())
 
+    def test_reading_file_returns_list_of_strings(self):
+        reader = FileReader('tests/data/content.txt')
+        result = list(reader.read_lines())
+        self.assertTrue(len(result) > 0)
